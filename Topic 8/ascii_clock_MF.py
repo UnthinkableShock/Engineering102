9eaa30a61d50cedb8dbea46676ed7866040883ce
Riley Hebert <99193191+UnthinkableShock@users.noreply.github.com>
2022-10-14 13:44:12 -0500

    added repository 8

---
+++ Topic 8/ascii_clock_MF.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Riley Hebert
+#               Triston Maresh
+#               Malcolm Ferguson
+#               Marissa Bosher
+# Section:      504
+# Assignment:   Topic 8 py file
+# Date:         14 October, 2022
+#
---
+++ Topic 8/ascii_clock_RH.py
@@ -0,0 +1,65 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Riley Hebert
+#               Triston Maresh
+#               Malcolm Ferguson
+#               Marissa Bosher
+# Section:      504
+# Assignment:   Topic 8 py file
+# Date:         14 October, 2022
+#
+
+myNumbers = [
+    ['000', 
+     '0 0', 
+     '0 0', 
+     '0 0', 
+     '000'],
+    [' 1 ',
+     '11 ',
+     ' 1 ',
+     ' 1 ',
+     '111',],
+    ['222',
+     '  2',
+     '222',
+     '2  ',
+     '222'],
+    ['333',
+     '  3',
+     '333',
+     '  3',
+     '333',],
+    ['4 4',
+     '4 4',
+     '444',
+     '  4',
+     '  4',],
+    ['555',
+     '5  ',
+     '555',
+     '  5',
+     '555',],
+    ['666',
+     '6  ',
+     '666',
+     '6 6',
+     '666',],
+    ['777',
+     '  7',
+     '  7',
+     '  7',
+     '  7',],
+    ['888',
+     '8 8',
+     '888',
+     '8 8',
+     '888',],
+    ['999',
+     '9 9',
+     '999',
+     '  9',
+     '  9',]
+]
