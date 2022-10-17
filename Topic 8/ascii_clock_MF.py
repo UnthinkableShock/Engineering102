00521f96006df60e4a43638fecb71cffbca577b3
Mal-3335 <buildingwithkieth@outlook.com>
2022-10-17 15:57:20 -0500

    works

--- Topic 8/ascii_clock_MF.py
+++ Topic 8/ascii_clock_MF.py
@@ -10,6 +10,8 @@
 # Assignment:   Topic 8 py file
 # Date:         14 October, 2022
 #
+#from ascii_clock_RH import myNumbers
+
 myNumbers = [
     ['000', 
      '0 0', 
@@ -60,8 +62,32 @@ myNumbers = [
      '9 9',
      '999',
      '  9',
-     '  9',]
+     '  9',],
+     ['   ',
+      ' : ',
+      '   ',
+      ' : ',
+      '   ']
 ]
 
+time = input("Time:")
+if len(time)<5:
+    time = "0"+time
+#seporate output into list
+#loop through list and replace strings with ints
+#place ints into numbers below
+newTime = []
+for x in range(len(time)):
+    if time[x] != ":":
+        newTime.append(int(time[x]))
+#print(newTime)
+one = myNumbers[newTime[0]]
+two = myNumbers[newTime[1]]
+seporator = myNumbers[10]
+three = myNumbers[newTime[2]]
+four = myNumbers[newTime[3]]
 
-print(myNumbers[1])
+if newTime[0] == 0:
+    for x in range(len(myNumbers[0])): print(two[x],seporator[x], three[x], four[x])
+else:
+    for x in range(len(myNumbers[0])): print(one[x], two[x],seporator[x], three[x], four[x])
