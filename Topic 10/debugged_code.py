d531dc9f0d7519d0e6071aad7146b72d215bc8ac
Mal-3335 <buildingwithkieth@outlook.com>
2022-11-02 16:17:44 -0500

    Co-authored-by: MarissaB2026 <MarissaB2026@users.noreply.github.com> Co-authored-by: TristonMaresh <TristonMaresh@users.noreply.github.com>

--- Topic 10/debugged_code.py
+++ Topic 10/debugged_code.py
@@ -21,21 +21,21 @@ def interpolate(x1, x2, y1, y2, x):
     '''This function interpolates between two points'''
     slope = (y2 - y1) / (x2 - x1)
     y = slope * (x - x1) + y1
-    return slope
+    return y#slope
 
 ##### 5 MPa Data #####
 # temperature in degrees C
 temp5 = [0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260]
 # specific volume in m^3/kg
-v5 = [0.0009977, 0.0009996, 0.0010057, 0.0010194, 0.0010267, 0.001041, 
-      0.0010576, 0.0010769, 0.0010988, 0.001124, 0.0011531, 0.0011868, 
+v5 = [0.0009977, 0.0009996, 0.0010057, 0.0010149, 0.0010267, 0.0010410, 
+      0.0010576, 0.0010769, 0.0010988, 0.0011240, 0.0011531, 0.0011868, 
       0.0012268, 0.0012755]
 # internal energy in kJ/kg
-u5 = [0.04, 83.61, 166.92, 250.29, 333.82, 417.65, 501.91, 586.8, 672.55,
-      759.47, 847.92, 938.39, 1031.6, 1128.5]
+u5 = [0.04, 83.61, 166.92, 250.29, 333.82, 417.65, 501.91, 586.80, 672.55,
+      759.47, 847.92, 938.39, 1031.60, 1128.50]
 # enthalpy in kJ/kg
 h5 = [5.03, 88.61, 171.95, 255.36, 338.96, 422.85, 507.19, 592.18, 
-      678.04, 765.09, 853.68, 944.32, 1037.7, 1134.9]
+      678.04, 765.09, 853.68, 944.32, 1037.70, 1134.90]
 # entropy in kJ/(kg K)
 s5 = [0.0001, 0.2954, 0.5705, 0.8287, 1.0723, 1.3034, 1.5236, 1.7344, 
       1.9374, 2.1338, 2.3251, 2.5127, 2.6983, 2.8841]
@@ -50,17 +50,17 @@ v10 = [0.0009952, 0.0009973, 0.0010035, 0.0010127, 0.0010244, 0.0010385,
        0.0012192, 0.0012653]
 # internal energy in kJ/kg
 u10 = [0.12, 83.31, 166.33, 249.43, 332.69, 416.23, 500.18, 584.72,
-       670.06, 756.48, 844.32, 934.01, 1026.2, 1121.6]
+       670.06, 756.48, 844.32, 934.01, 1026.20, 1121.60]
 # enthalpy in kJ/kg
 h10 = [10.07, 93.28, 176.37, 259.55, 342.94, 426.62, 510.73, 595.45,
-       681.01, 767.68, 855.8, 945.82, 1038.3, 1134.3]
+       681.01, 767.68, 855.80, 945.82, 1038.30, 1134.30]
 # entropy in kJ/(kg K)
-s10 = [0.0003, 0.2943, 0.5685, 0.826, 1.0691, 1.2996, 1.5191, 1.7293,
-       1.9316, 2.1271, 2.3174, 2.5037, 2.6876, 2.871]
+s10 = [0.0003, 0.2943, 0.5685, 0.8260, 1.0691, 1.2996, 1.5191, 1.7293,
+       1.9316, 2.1271, 2.3174, 2.5037, 2.6876, 2.8710]
 
 ##### do calculations #####
 # get temperature and pressure from user
-temp = int(input("Enter a temperature between 0 and 260 deg C: "))
+temp = float(input("Enter a temperature between 0 and 260 deg C: "))
 pres = float(input("Enter a pressure between 5 and 10 MPa: "))
 
 # loop over temperatures in list to find the two that bound
@@ -96,8 +96,8 @@ h = interpolate(5, hl, 10, hh, pres)
 s = interpolate(5, sl, 10, sh, pres)
 
 ##### print results #####
-print('Properties at {pres} deg C and {temp} MPa are:')
+print(f'Properties at {temp} deg C and {pres} MPa are:')
 print(f'Specific volume (m^3/kg): {v:.7f}')
 print(f'Specific internal energy (kJ/kg): {u:.2f}')
 print(f'Specific enthalpy (kJ/kg): {h:.2f}')
-print(f'Specific entropy (kJ/kgK): {s:.3f}')
+print(f'Specific entropy (kJ/kgK): {s:.4f}')
