a260641541d442d30d2b331d7816efdd3c0e6942
Riley Hebert <99193191+UnthinkableShock@users.noreply.github.com>
2022-11-19 15:12:43 -0600

    did module 12 team lab. Please please PLEASE ask me if you have questions or don't understand.

--- Topic 12/matplotlib_example.py
+++ Topic 12/matplotlib_example.py
@@ -14,4 +14,37 @@
 # As a team, we have gone through all required sections of the  
 # tutorial, and each team member understands the material 
 
+
 import matplotlib as mp
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-2, 2, 100) #np.linspace(start, end, length), generates a 1D array of x values to use
+
+def equation(x, f):
+    return (1/4)/f*x**2
+
+fig, ax = plt.subplots() # weird python unpacking thingy
+f = 2
+ax.plot(x, equation(x, f), c='r', lw=f, label=f'f={f}')
+# c is shorthand for color, which would also work
+# lw is shorthand for linewidth, which would also work
+# label= defines the label, have to give it a string
+f = 6
+ax.plot(x, equation(x, f), c='b', lw=f, label=f'f={f}')
+ax.legend() # adds any labels defined
+
+x2 = np.linspace(-4, 4, 25) # generates a 1D array of x values to use
+def y(x):
+    return 2*x**3 + 3*x**2 - 11*x - 6
+fig2, ax2 = plt.subplots()
+ax2.plot(x2, y(x2), '*', markersize=10, markerfacecolor=(1, 1, 0, 1), markeredgecolor='k')
+
+
+x3 = np.linspace(-6, 6, 100)
+fig3, (ax3, ax4) = plt.subplots(2, 1) # 2, 1 = 2 rows, 1 column
+ax3.set_title("Plot of cos(x) and sin(x)")
+ax3.plot(x3, np.cos(x3), c='r', label='cos(x)')
+ax4.plot(x3, np.sin(x3), c='b', label='sin(x)')
+ax3.legend()
+ax4.legend()
--- Topic 12/numpy_example.py
+++ Topic 12/numpy_example.py
@@ -14,13 +14,25 @@
 # As a team, we have gone through all required sections of the  
 # tutorial, and each team member understands the material 
 
-import numpy as np
+import numpy as giraffe
 
 # important commands
+# np.array
 # arange
-# reshape
+# .shape
+# .reshape
 # limspace
 # size
-# shape
 # array
 # @ is matrix multiplication
+# .transpose()
+
+A = giraffe.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]]) # needs to be 3 x 4
+B = giraffe.array([[0, 1], [2, 3], [4, 5], [6, 7]]) # needs to be 4 x 2
+C = giraffe.array([[0, 1, 2], [3, 4, 5]]) # needs to be 2 x 3
+
+D = A @ B @ C
+TD = D.transpose()
+E = D**(1/2) / 2
+
+print(f"A = {A}\n\nB = {B}\n\nC = {C}\n\nD = {D}\n\nD^T = {TD}\n\nE = {E}")
